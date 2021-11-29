56ae49e5d7dd5f703ebdae6352823e1e05f7fdc6
David Yauri <david.yauri@tecsup.edu.pe>
2021-11-29 05:24:03 +0100

    first commit

---
+++ Entity Framework Core/Entity Framework Core/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Entity_Framework_Core.DataContext;
+using Entity_Framework_Core.Interfaces;
+using Entity_Framework_Core.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Entity_Framework_Core
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            GetContext().Database.EnsureCreated();
+            MainPage = new NavigationPage(new BooksPage());
+        }
+        public static AppDbContext GetContext()
+        {
+            string DbPath = DependencyService.Get<IConfigDataBase>().GetFullPath("efCoreya.db");
+
+            return new AppDbContext(DbPath);
+        }
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+
+    }
+}
